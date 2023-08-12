82884e31cd601d03a2d0117ed5cd340870db165c
gigisoza <sozashviligigi@gmail.com>
2023-08-12 21:47:06 +0400

    Create search bar and dropdown in Countries.tsx

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -3,6 +3,26 @@ import Article from "./Article";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const regions = [
+    {
+      name: "Europe",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "Oceania",
+    },
+    {
+      name: "Americas",
+    },
+    {
+      name: "Antarctic",
+    },
+  ];
 
   useEffect(() => {
     const getCountries = async () => {
@@ -26,6 +46,33 @@ export default function Countries() {
         </h1>
       ) : (
         <section className="container mx-auto p-8">
+          <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
+            <form autoComplete="off" className="max-w-4xl md:flex-1">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search for a country by its name"
+                required
+                className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"
+              />
+            </form>
+
+            <form>
+              <select
+                name="filter-by-region"
+                id="filter-by-region"
+                className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700"
+              >
+                {regions.map((region, index) => (
+                  <option key={index} value={region.name}>
+                    {region.name}
+                  </option>
+                ))}
+              </select>
+            </form>
+          </div>
+
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
             {countries.map((country) => (
               <Article key={country.name.common} {...country} />
