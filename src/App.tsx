a7b7655562c0f6512d6d2a6a3f3106d2deb655a8
gigisoza <sozashviligigi@gmail.com>
2023-08-27 22:17:34 +0400

    Add home page button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Countries />}></Route>
           <Route path="/:name" element={<SingleCountry />}></Route>
